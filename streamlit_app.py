7c230785d718bc1be2bc7355137a444c74ecafe0
legosnowman <patrick.cp.lai@gmail.com>
2024-04-01 02:11:55 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -32,7 +32,8 @@ def hk_weather():
     #print(type(response_json))
     if response == 200:
         response_json = response.json()
-        st.write(response_json['generalSituation'])
+        #st.write(response_json['generalSituation'])
+        st.write(response_json["forecastPeriod"])
         
         #if response_json['data'][0] is not None:
         #    epi_one  = response_json['data'][0]
